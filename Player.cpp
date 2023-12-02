3b4728a04bdb35eec69570c6d855357f9b2df76d
chowb15 <144254961+chowb15@users.noreply.github.com>
2023-12-02 12:16:08 -0500

    collision in new function

--- Player.cpp
+++ Player.cpp
@@ -47,6 +47,31 @@ char Player::getPlayerS()
     return headPos.symbol;
 }
 
+bool Player::checkSelfCollision()
+{
+    objPos currHead;
+    playerPosList->getHeadElement(currHead);
+
+    for(int i = 1; i < playerPosList->getSize(); i++)
+    {
+        playerPosList->getElement(headPos, i);
+        if(currHead.x == headPos.x && currHead.y == headPos.y)
+            return true;
+    }
+    return false;
+}
+
+bool Player::checkFoodConsumption()
+{
+    objPos currHead;
+    playerPosList->getHeadElement(currHead);
+
+    if(currHead.x == food->getFoodX() && currHead.y == food->getFoodY())
+        return true;
+    return false;
+    
+}
+
 
 
 void Player::updatePlayerDir()
@@ -116,21 +141,19 @@ void Player::movePlayer()
             break;
     }
 
-    for(int i = 1; i < playerPosList->getSize(); i++)
+    if(checkSelfCollision())
     {
-        playerPosList->getElement(headPos, i);
-        if(currHead.x == headPos.x && currHead.y == headPos.y){
-            mainGameMechsRef->setExitTrue();
-            mainGameMechsRef->setLoseTrue();
-            }
+        mainGameMechsRef->setExitTrue();
+        mainGameMechsRef->setLoseTrue();
     }
-    
-    // new current head should be inserted to head of list, then remove tail
-    if(currHead.x == food->getFoodX() && currHead.y == food->getFoodY()){
+    if(checkFoodConsumption()){
         playerPosList->insertHead(currHead);
         food->generateFood(playerPosList);
         mainGameMechsRef->incrementScore();
     }
+    
+    // new current head should be inserted to head of list, then remove tail
+    
     else{
         playerPosList->insertHead(currHead);
         playerPosList->removeTail();
